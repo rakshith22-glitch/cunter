550bbf99d626b88b68aabc0327c8a4f5920bd5aa
rakshith22-glitch <rakshith22@outlook.com>
2024-06-11 17:00:34 -0700

    report

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,39 @@
+// import React from "react";
+// import "../styles.css";
+// import iamhere from "../assets/iamhere.webp";
+// import giphy from "../assets/giphy.webp";
+// import meme1 from "../assets/meme1.jpg";
+
+// const Dashboard = () => {
+//   return (
+//     <div className="dashboard">
+//       {/* <div id="dexscreener-embed"> */}
+//         {/* <iframe 
+//           src="https://dexscreener.com/solana/FJ6MdHqFwmnzx2g19s6X8NDbF7gZCnU2yE1rKd9vbnwf?embed=1&theme=dark&trades=1&info=1" 
+//           title="Dexscreener Chart"
+//         ></iframe> */}
+//         <h1>Most Awaited Hunter Biden Coin Launching Soon .........</h1>
+       
+//       {/* </div> */}
+//       <div className="card-container">
+//         <div className="card">
+//           <img src={iamhere} alt="Loading Screen" className="card-image" />
+//         </div>
+//         <div className="card">
+//           <img src={giphy} alt="Desktop" className="card-image" />
+//         </div>
+
+//         <div className="card">
+//           <img src={meme1} alt="Desktop" className="card-image" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles.css";
 import iamhere from "../assets/iamhere.webp";
 import giphy from "../assets/giphy.webp";
@@ -7,12 +42,10 @@ import meme1 from "../assets/meme1.jpg";
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <div id="dexscreener-embed">
-        <iframe 
-          src="https://dexscreener.com/solana/FJ6MdHqFwmnzx2g19s6X8NDbF7gZCnU2yE1rKd9vbnwf?embed=1&theme=dark&trades=1&info=1" 
-          title="Dexscreener Chart"
-        ></iframe>
-      </div>
+         <Link to="/" className="nav-button">
+         Home
+        </Link>
+      <h1>Most Awaited Hunter Biden Coin Launching Soon .........</h1>
       <div className="card-container">
         <div className="card">
           <img src={iamhere} alt="Loading Screen" className="card-image" />
@@ -20,7 +53,6 @@ const Dashboard = () => {
         <div className="card">
           <img src={giphy} alt="Desktop" className="card-image" />
         </div>
-
         <div className="card">
           <img src={meme1} alt="Desktop" className="card-image" />
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaTwitter, FaTelegramPlane, FaInstagram, FaTiktok } from 'react-icons/fa';
+import {
+  FaTwitter,
+  FaTelegramPlane,
+  FaInstagram,
+  FaTiktok,
+} from "react-icons/fa";
 import "../styles.css";
 import profile from "../assets/profile.jpg";
 import background from "../assets/background.jpg";
@@ -11,25 +16,43 @@ const Header = () => {
       <div className="header-top">
         <img src={profile} alt="User" className="user-image" />
         <nav className="nav">
-          {/* <Link to="/dashboard">Chart & More </Link> */}
-          {/* <Link to="/tokenomics">Tokenomics</Link>
-          <Link to="/roadmap">Roadmap</Link>
+           <Link to="https://www.nbcnews.com/politics/justice-department/verdict-reached-hunter-biden-gun-trial-rcna155458">NEWS</Link> *
+           <Link to="https://bidenreport.com/#p=1">MY STORY</Link>
+          {/* <Link to="/roadmap">Roadmap</Link>
           <Link to="/products">Products</Link>
-          <Link to="/gallery">Gallery</Link> */}
+          <Link to="/gallery">Gallery</Link> * */}
         </nav>
         <div className="social-icons">
-        <a href="https://x.com/cunterbiden_sol" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-        <a href="https://t.me/cunter_biden/7" target="_blank" rel="noopener noreferrer"><FaTelegramPlane /></a>
-        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-        {/* <a href="https://www.tiktok.com/@wafflesj15?_t=8mRgnzllQIf&_r=1" target="_blank" rel="noopener noreferrer"><FaTiktok /></a> */}
-      </div>
+          <a
+            href="https://x.com/cunterbiden_sol"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://t.me/cunter_biden/7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTelegramPlane />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+          {/* <a href="https://www.tiktok.com/@wafflesj15?_t=8mRgnzllQIf&_r=1" target="_blank" rel="noopener noreferrer"><FaTiktok /></a> */}
+        </div>
       </div>
       <div className="header-content">
-        <h1 className="site-title">$CUNTER</h1>
-        <Link to="/dashboard" className="nav-button">Buy now</Link>
+        <Link to="/dashboard" className="nav-button">
+          <p className="site-title">$CUNTER</p>
+        </Link>
       </div>
       <img src={background} alt="Header" className="header-image" />
-     
     </header>
   );
 };
